d50f3823cc022abe040d75bc73f833041bec5fed
majorazero <daniel.cw.hsu@gmail.com>
2018-10-15 23:28:31 -0700

    improved styling and formatting

--- controllers/scraperAPIRoutes.js
+++ controllers/scraperAPIRoutes.js
@@ -4,7 +4,34 @@ const axios = require("axios");
 
 module.exports = function(app){
   app.post("/api/scrape",function(req,res){
-    console.log(req.body.url);
-    res.json("BYE!");
-  })
+    //we'll use axios to grab the site link
+    axios.get(req.body.url).then(function(response){
+      //we'll use cheerio here to start scraping
+      let $ =  cheerio.load(response.data);
+
+      let results = [];
+
+      $("h5").each(function(i,element){
+        let title = $(element).find("a").text();
+        let link = $(element).find("a").attr("href");
+        let blurb = $(element).parent().find("p").text();
+        if(blurb === ""){
+          blurb = "No Blurb";
+        }
+        let author = $(element).parent().find("a .uppercase").text().replace(/([A-Z])/g,' $1');
+        if(author === ""){
+          author = "No Author Data";
+        }
+
+        results.push({
+          title: title,
+          link: link,
+          blurb: blurb,
+          author: author
+        });
+      });
+
+      res.json(results);
+    });
+  });
 };
--- public/javascript/app.js
+++ public/javascript/app.js
@@ -6,6 +6,30 @@ $("#scrapeButton").on("click",function(){
       url: $("#scrapeOption").val()
     }
   }).then(function(data){
-    console.log(data);
+    let newTopic = $("#scrapeOption").val().replace("http://www.latimes.com/","").replace("/","");
+    newTopic = newTopic.charAt(0).toUpperCase()+newTopic.substring(1);
+    if(newTopic === ""){
+      newTopic = "Home";
+    }
+    $("#currentPage").text(newTopic);
+    contentBuilder(data);
   });
 });
+
+
+function contentBuilder(data){
+  for(let i = 0; i < data.length; i++){
+    let wrapper = $("<div>").addClass("scrapedArticles");
+    let link = $("<a>").attr("href","http://www.latimes.com"+data[i].link);
+    link.append("<h3>"+data[i].title+"</h3>");
+    wrapper.append(link);
+    wrapper.append("<h4>By: "+data[i].author+"</h4>");
+    wrapper.append("<h5>"+data[i].blurb+"</h5>");
+    let button = $("<button>").text("Show Notes");
+    button.on("click",function(){
+      //noteBuilder();
+    });
+    wrapper.append(button);
+    $("#scraperBlock").append(wrapper);
+  }
+}
